e67c453dbf4c1ee99d03ddf7086ca76843e562b8
Pandula <pandula2011@gmail.com>
2021-02-14 16:27:24 +0100

    new experiments created for different parameter configs, cleaned up folder structure

---
+++ Pb_policy_iteration/cart_pole_modified_env/modified_algo_analysis/mod_vs_original_algo_experiments/experiments/run_modified_experiments.bash
@@ -0,0 +1,8 @@
+# First, install the custom cart-pole environment
+pip install -e ../pbpi/custom_cartpole/. &&
+
+# Then, run the python experiments
+python exp_modified_config_1.py &
+python exp_modified_config_2.py &
+python exp_modified_config_3.py &
+python exp_modified_config_4.py
--- Pb_policy_iteration/cart_pole_modified_env/modified_algo_analysis/mod_vs_original_algo_experiments/experiments/run_original_experiments.bash
+++ Pb_policy_iteration/cart_pole_modified_env/modified_algo_analysis/mod_vs_original_algo_experiments/experiments/run_original_experiments.bash
@@ -2,4 +2,7 @@
 pip install -e ../pbpi/custom_cartpole/. &&
 
 # Then, run the python experiments
-python experiment_1.py
+python exp_orginal_config_1.py &
+python exp_orginal_config_2.py &
+python exp_orginal_config_3.py &
+python exp_orginal_config_4.py
