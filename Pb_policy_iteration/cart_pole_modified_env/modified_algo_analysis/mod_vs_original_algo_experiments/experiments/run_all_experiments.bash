bfec117613e10c0dba1c2f00a036248b22911185
Pandula <pandula2011@gmail.com>
2021-02-21 12:49:51 +0100

    changed the error in file names in the run_all_experiments.bash file

--- Pb_policy_iteration/cart_pole_modified_env/modified_algo_analysis/mod_vs_original_algo_experiments/experiments/run_all_experiments.bash
+++ Pb_policy_iteration/cart_pole_modified_env/modified_algo_analysis/mod_vs_original_algo_experiments/experiments/run_all_experiments.bash
@@ -4,14 +4,12 @@ pip install -e ../pbpi/custom_cartpole/. &&
 # Then, run the python experiments
 python exp_modified_config_1.py &
 python exp_modified_config_2.py &
-
-python exp_orginal_config_5.py &
-python exp_orginal_config_6.py &
-python exp_orginal_config_7.py &
-python exp_orginal_config_8.py &
-python exp_orginal_config_9.py &
-python exp_orginal_config_10.py &
-
+python exp_original_config_5.py &
+python exp_original_config_6.py &
+python exp_original_config_7.py &
+python exp_original_config_8.py &
+python exp_original_config_9.py &
+python exp_original_config_10.py &
 python exp_modified_config_5.py &
 python exp_modified_config_6.py &
 python exp_modified_config_7.py &
